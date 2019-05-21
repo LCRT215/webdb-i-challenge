7010f40b4d83ea3595d2add4d07bfd5f52c81800
Leslie Thompson <lcrthompson@gmail.com>
2019-05-21 09:04:49 -0700

    adding endpoints to server.js

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
+const accounts = require("./data/accounts-model");
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+
 // your code here
+
+server.get("/", async (req, res) => {
+  try {
+    const data = await accounts.find();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+});
+
+server.get("/:id", async (req, res) => {
+  try {
+    const data = await accounts.findById();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error" });
+  }
+});
 
 module.exports = server;
